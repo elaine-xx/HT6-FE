854dc81ec202e2f18e50b2a5d0128471549df2e7
Devon Fazekas <fazekas.devon@gmail.com>
2023-08-19 18:58:23 -0400

    Abstract home content to dedicated page

---
+++ lib/recorder.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecorderScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recorder Screen'),
+      ),
+      body: Center(
+        child: Text('Hello world'),
+      ),
+    );
+  }
+}
