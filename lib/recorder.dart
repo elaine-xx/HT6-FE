51cdb6fde10d4e27ff1f8a7ebf0c4a6502c7d043
Devon Fazekas <fazekas.devon@gmail.com>
2023-08-20 00:01:08 -0400

    Add TTS to recorder page

--- lib/recorder.dart
+++ lib/recorder.dart
@@ -1,7 +1,70 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class RecorderScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+class RecorderScreen extends StatefulWidget {
   const RecorderScreen({super.key});
+
+  @override
+  State<RecorderScreen> createState() => _RecorderScreenState();
+}
+
+class _RecorderScreenState extends State<RecorderScreen> {
+  bool _hasSpeech = false;
+  String lastWords = '';
+  String lastError = '';
+  String lastStatus = '';
+  double level = 0.0;
+  double minSoundLevel = 50000;
+  double maxSoundLevel = -50000;
+  bool _onDevice = false;
+  String _currentLocaleId = '';
+  final TextEditingController _pauseForController =
+      TextEditingController(text: '3');
+  final TextEditingController _listenForController =
+      TextEditingController(text: '30');
+  final SpeechToText speech = SpeechToText();
+
+  // This initializes SpeechToText. That only has to be done
+  /// once per application, though calling it again is harmless
+  /// it also does nothing. The UX of the sample app ensures that
+  /// it can only be called once.
+  Future<void> initSpeechState() async {
+    try {
+      var hasSpeech = await speech.initialize(
+          onError: errorListener, onStatus: statusListener);
+
+      if (!hasSpeech) {
+        print("The user has denied the use of speech recognition.");
+      }
+
+      if (!mounted) return;
+
+      setState(() {
+        _hasSpeech = hasSpeech;
+      });
+    } catch (e) {
+      setState(() {
+        lastError = 'Speech recognition failed: ${e.toString()}';
+        _hasSpeech = false;
+      });
+    }
+  }
+
+  void errorListener(SpeechRecognitionError error) {
+    setState(() {
+      lastError = '${error.errorMsg} - ${error.permanent}';
+    });
+  }
+
+  void statusListener(String status) {
+    setState(() {
+      lastStatus = status;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +84,13 @@ class RecorderScreen extends StatelessWidget {
             ),
             clipBehavior: Clip.antiAlias,
             decoration: const BoxDecoration(color: Color(0xFFF4F4F4)),
-            child: const Column(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                InitSpeechWidget(_hasSpeech, initSpeechState),
+                SpeechControlWidget(_hasSpeech, speech.isListening,
+                    startListening, stopListening, cancelListening),
                 RecordingIcon(),
                 SizedBox(height: 46),
                 TextBubble(),
@@ -34,6 +100,63 @@ class RecorderScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  // This is called each time the users wants to start a new speech
+  // recognition session
+  void startListening() {
+    lastWords = '';
+    lastError = '';
+    final pauseFor = int.tryParse(_pauseForController.text);
+    final listenFor = int.tryParse(_listenForController.text);
+    // Note that `listenFor` is the maximum, not the minimum, on some
+    // systems recognition will be stopped before this value is reached.
+    // Similarly `pauseFor` is a maximum not a minimum and may be ignored
+    // on some devices.
+    speech.listen(
+      onResult: resultListener,
+      listenFor: Duration(seconds: listenFor ?? 30),
+      pauseFor: Duration(seconds: pauseFor ?? 3),
+      partialResults: true,
+      localeId: _currentLocaleId,
+      onSoundLevelChange: soundLevelListener,
+      cancelOnError: true,
+      listenMode: ListenMode.confirmation,
+      onDevice: _onDevice,
+    );
+    setState(() {});
+  }
+
+  void stopListening() {
+    speech.stop();
+    setState(() {
+      level = 0.0;
+    });
+  }
+
+  void cancelListening() {
+    speech.cancel();
+    setState(() {
+      level = 0.0;
+    });
+  }
+
+  /// This callback is invoked each time new recognition results are
+  /// available after `listen` is called.
+  void resultListener(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = '${result.recognizedWords} - ${result.finalResult}';
+      print(lastWords);
+    });
+  }
+
+  void soundLevelListener(double level) {
+    minSoundLevel = min(minSoundLevel, level);
+    maxSoundLevel = max(maxSoundLevel, level);
+    // _logEvent('sound level $level: $minSoundLevel - $maxSoundLevel ');
+    setState(() {
+      this.level = level;
+    });
   }
 }
 
@@ -84,5 +207,61 @@ class RecordingIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Icon(Icons.mic, size: 60);
+  }
+}
+
+class InitSpeechWidget extends StatelessWidget {
+  const InitSpeechWidget(this.hasSpeech, this.initSpeechState, {Key? key})
+      : super(key: key);
+
+  final bool hasSpeech;
+  final Future<void> Function() initSpeechState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        TextButton(
+          onPressed: hasSpeech ? null : initSpeechState,
+          child: const Text('Initialize'),
+        ),
+      ],
+    );
+  }
+}
+
+/// Controls to start and stop speech recognition
+class SpeechControlWidget extends StatelessWidget {
+  const SpeechControlWidget(this.hasSpeech, this.isListening,
+      this.startListening, this.stopListening, this.cancelListening,
+      {Key? key})
+      : super(key: key);
+
+  final bool hasSpeech;
+  final bool isListening;
+  final void Function() startListening;
+  final void Function() stopListening;
+  final void Function() cancelListening;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        TextButton(
+          onPressed: !hasSpeech || isListening ? null : startListening,
+          child: const Text('Start'),
+        ),
+        TextButton(
+          onPressed: isListening ? stopListening : null,
+          child: const Text('Stop'),
+        ),
+        TextButton(
+          onPressed: isListening ? cancelListening : null,
+          child: const Text('Cancel'),
+        )
+      ],
+    );
   }
 }
